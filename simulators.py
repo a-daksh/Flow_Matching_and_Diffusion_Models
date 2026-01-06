9c2b79c81e978bcc3aba3545f239fbf66af0bb68
Daksh Adhar <dakshadhar1810@gmail.com>
2026-01-06 18:42:43 +0530

    JAX

--- simulators.py
+++ simulators.py
@@ -1,4 +1,5 @@
-import torch
+import jax
+import jax.numpy as jnp
 from base import Simulator, ODE, SDE
 from models import ConditionalVectorField
 
@@ -6,22 +7,22 @@ class EulerSimulator(Simulator):
     def __init__(self, ode: ODE):
         self.ode = ode
 
-    def step(self, xt: torch.Tensor, t: torch.Tensor, h: torch.Tensor, **kwargs):
+    def step(self, xt: jax.Array, t: jax.Array, h: jax.Array, key: jax.Array, **kwargs) -> jax.Array:
         return xt + self.ode.drift_coefficient(xt,t, **kwargs) * h
 
 class EulerMaruyamaSimulator(Simulator):
     def __init__(self, sde: SDE):
         self.sde = sde
 
-    def step(self, xt: torch.Tensor, t: torch.Tensor, h: torch.Tensor, **kwargs):
-        return xt + self.sde.drift_coefficient(xt,t, **kwargs) * h + self.sde.diffusion_coefficient(xt,t, **kwargs) * torch.sqrt(h) * torch.randn_like(xt)
+    def step(self, xt: jax.Array, t: jax.Array, h: jax.Array, key: jax.Array, **kwargs) -> jax.Array:
+        return xt + self.sde.drift_coefficient(xt,t, **kwargs) * h + self.sde.diffusion_coefficient(xt,t, **kwargs) * jnp.sqrt(h) * jax.random.normal(key, shape=xt.shape)
 
 class CFGVectorFieldODE(ODE):
     def __init__(self, net: ConditionalVectorField, guidance_scale: float = 1.0):
         self.net = net
         self.guidance_scale = guidance_scale
 
-    def drift_coefficient(self, x: torch.Tensor, t: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+    def drift_coefficient(self, x: jax.Array, t: jax.Array, y: jax.Array, **kwargs) -> jax.Array:
         """
         Args:
         - x: (bs, c, h, w)
@@ -29,6 +30,6 @@ class CFGVectorFieldODE(ODE):
         - y: (bs,)
         """
         guided_vector_field = self.net(x, t, y)
-        unguided_y = torch.ones_like(y) * 10
+        unguided_y = jnp.ones_like(y) * 10
         unguided_vector_field = self.net(x, t, unguided_y)
         return (1 - self.guidance_scale) * unguided_vector_field + self.guidance_scale * guided_vector_field
